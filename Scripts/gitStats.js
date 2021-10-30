e8d6df83d9ecac947688aa9b9706d7a83035a56b
Ben Landon <81256789+https123456789@users.noreply.github.com>
2021-10-30 04:26:50 +0000

    Fixed issue with determining if cached data should be used.

--- Scripts/gitStats.js
+++ Scripts/gitStats.js
@@ -7,41 +7,52 @@ function getContribsFor(username, callback) {
 		"shas": [
 
 		]
-	}	
-	if (checkIfGAPICallsExist()) {
-		var req = new XMLHttpRequest();
-		req.addEventListener("load", function() {
-			if (this.status == 200) {
-				var authorCount = 0;
-				var json = JSON.parse(this.responseText);
-				for (var i = 0; i < json.length; i++) {
-					if (json[i]["author"]["login"] == username) {
-						authorCount += 1;
-					}
-				}
-				ret.count = authorCount;
-				ret.percent = Math.round((ret.count / json.length) * 100) / 100;
-				callback(ret);
-			}
-		});
-		req.open("GET", "https://api.github.com/repos/https123456789/Textual/commits");
-		req.send();
-	} else {
-		// Use Cached API Data
-		var cachedAPIData = JSON.parse(localStorage.getItem("GithubAPIData"));
-		ret.count = cachedAPIData["Contributors"][username]["count"];
-		ret.percent = Math.round((ret.count / cachedAPIData["Contributors-Overall"]["Total-Commits"]) * 100) / 100;
-		callback(ret);
 	}
+	checkIfGAPICallsExist(function(useCached) {
+		console.log(useCached);
+		if (useCached) {
+			var req = new XMLHttpRequest();
+			req.addEventListener("load", function() {
+				if (this.status == 200) {
+					var authorCount = 0;
+					var json = JSON.parse(this.responseText);
+					for (var i = 0; i < json.length; i++) {
+						if (json[i]["author"]["login"] == username) {
+							authorCount += 1;
+						}
+					}
+					ret.count = authorCount;
+					ret.percent = Math.round((ret.count / json.length) * 100) / 100;
+					callback(ret);
+				}
+			});
+			req.open("GET", "https://api.github.com/repos/https123456789/Textual/commits?type=all");
+			req.send();
+		} else {
+			// Use Cached API Data
+			console.warn("Using cached Github API data.");
+			var cachedAPIData = JSON.parse(localStorage.getItem("GithubAPIData"));
+			ret.count = cachedAPIData["Contributors"][username]["count"];
+			ret.percent = Math.round((ret.count / cachedAPIData["Contributors-Overall"]["Total-Commits"]) * 100) / 100;
+			callback(ret);
+		}
+	});
 }
-/*{"Contributors":{"https123456789":{"count":29},"LGgameLAB":{"count":1}},"Contributors-Overall": {"Total-Commits": 30}}*/
-function checkIfGAPICallsExist() {
+function checkIfGAPICallsExist(callback) {
 	var res = new XMLHttpRequest();
 	res.addEventListener("load", function() {
 		if (this.status == 200) {
-			return false;
+			var d = JSON.parse(this.responseText);
+			console.log(d.resources.core.remaining);
+			if (d.resources.core.remaining != 0) {
+				console.log(true);
+				callback(true);
+			} else {
+				console.log(false);
+				callback(false);
+			}
 		} else {
-			return false;
+			callback(false);
 		}
 	});
 	res.open("GET", "https://api.github.com/rate_limit", false);
