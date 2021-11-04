f901de6aff3bdf4db0f6df3626696ffc1fc07871
Ben Landon <81256789+https123456789@users.noreply.github.com>
2021-11-04 02:06:10 +0000

    Fixed issue with content not being stripped of html artifacts.

--- Scripts/fileExport.js
+++ Scripts/fileExport.js
@@ -3,10 +3,19 @@ class Exporter {
 		this.fileName = f.name;
 		this.fileExtension = f.extension;
 	}
-	convertFromRawHTML(content) {
+	convertFromRawHTML(content, ops) {
 		var formattedContent = content;
+		// Create real symbols
 		formattedContent = formattedContent.replaceAll("&lt;", "<");
 		formattedContent = formattedContent.replaceAll("&gt;", ">");
+		if (ops["remove-div"]) {
+			formattedContent = formattedContent.replaceAll("<div>", "");
+			formattedContent = formattedContent.replaceAll("</div>", "");
+		}
+		if (ops["remove-br"]) {
+			formattedContent = formattedContent.replaceAll("<br>", "");
+		}
+		console.log(formattedContent);
 		return formattedContent;
 	}
 }
@@ -17,7 +26,10 @@ class PlainTextExporter extends Exporter {
 		this.content = content;
 	}
 	exportToFile() {
-		var formattedContent = this.convertFromRawHTML(this.content);
+		var formattedContent = this.convertFromRawHTML(this.content, {
+			"remove-div": true,
+			"remove-br": true
+		});
 		var blob = new Blob([
 			formattedContent
 		], {
@@ -36,7 +48,10 @@ class HTMLExporter extends Exporter {
 		this.content = content;
 	}
 	exportToFile() {
-		var formattedContent = this.convertFromRawHTML(this.content);
+		var formattedContent = this.convertFromRawHTML(this.content, {
+			"remove-div": true,
+			"remove-br": true
+		});
 		var blob = new Blob([
 			formattedContent
 		], {
